be39ad51d6f83ae8d8a6bab2d21ac53400dbf497
julian <sjulian9409@gmail.com>
2020-05-28 18:29:31 -0500

    first commit

---
+++ src/main/java/com/microservices/tablesservice/tablesservice/TablesserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.tablesservice.tablesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablesserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablesserviceApplication.class, args);
+	}
+
+}
